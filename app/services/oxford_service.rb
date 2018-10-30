a3e4fbd3a5d40b6fb5cfed4a1a9d9653e4b500c6
MacInnes <califormula@gmail.com>
2018-10-30 10:11:18 -0600

    service is working properly

--- app/services/oxford_service.rb
+++ app/services/oxford_service.rb
@@ -1,24 +1,22 @@
 class OxfordService
 
   def get_sentences(word)
-    get_json("/entries/en/#{word.downcase}/sentences")
+    get_json("api/v1/entries/en/#{word.downcase}/sentences")
   end
 
   private
 
   def conn
-    Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1") do |faraday|
+    Faraday.new(url: "https://od-api.oxforddictionaries.com/") do |faraday|
       faraday.headers["app_id"] = ENV["OXFORD_APP_ID"]
       faraday.headers["app_key"] = ENV["OXFORD_API_KEY"]
-      faraday.headers["Content-Type"] = "application/json"
-      faraday.headers["Accept"] = "application/json"
       faraday.adapter Faraday.default_adapter
     end
   end
 
   def get_json(url)
-    binding.pry
     result = JSON.parse(conn.get(url).body, symbolize_names: true)
+    binding.pry
   end
 
 end
