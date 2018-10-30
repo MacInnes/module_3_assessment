baab10d927bb2516a447f0c5d48c2266e8e51de7
MacInnes <califormula@gmail.com>
2018-10-30 11:19:05 -0600

    refactors game serializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -8,21 +8,23 @@ class GameSerializer < ActiveModel::Serializer
   def scores
     user_1 = User.find(object.player_1.id)
     user_2 = User.find(object.player_2.id)
-    output_hash = [{
-      user_id: user_1.id,
-      score: player_score(object, user_1)
-    },
-    {
-      user_id: user_2.id,
-      score: player_score(object, user_2)
-    }]
+    output_hash = [user_data(user_1), user_data(user_2)]
   end
 
-  def player_score(game, player)
-    plays = Play.where("game_id": game.id, "user_id": player.id)
+  private
+
+  def player_score(player)
+    plays = Play.where("game_id": object.id, "user_id": player.id)
     plays.inject(0) do |total, play|
       total += play.score
     end
+  end
+
+  def user_data(user)
+    {
+      user_id: user.id,
+      score: player_score(user)
+    }
   end
 
 end
