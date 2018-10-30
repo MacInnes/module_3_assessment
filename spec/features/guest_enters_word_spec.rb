5c07d6178d43b9177280573dc1cecfe3db26587c
MacInnes <califormula@gmail.com>
2018-10-30 09:25:16 -0600

    initial user story test written

---
+++ spec/features/guest_enters_word_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'a guest user' do
+  scenario 'enters a word on the page' do
+    visit '/'
+    fill_in "word", with: "mindfulness"
+    click_on "Submit"
+
+    expect(page).to have_content("Examples for using 'mindfulness'")
+    expect(page).to have_css(".sentences")
+    expect(page).to have_content("Region: British")
+    expect(page).to have_content("Region: Canadian")
+    expect(page).to_not have_content("Region: North American")
+  end
+end
